cb9611b2c5879db963dadc1c8ff7bd4a260f1dc6
Bilguun Tegshbaikh <bilguunzen@gmail.com>
2018-10-26 14:00:37 +0800

    changed model into h5

--- fr_utils.py
+++ fr_utils.py
@@ -186,8 +186,7 @@ def load_dataset():
 def img_to_encoding_top(image, model):
     side = min(image.shape[0], image.shape[1])
     image = image[0:side, 0:side]
-    image = cv2.resize(image, (96, 96))
-    img = image[...,::-1]
+    img = cv2.resize(image, (96, 96))
     img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
     x_train = np.array([img])
     embedding = model.predict_on_batch(x_train)
@@ -198,8 +197,7 @@ def img_to_encoding_bot(image, model):
     s1 = image.shape[0] - side
     s2 = image.shape[1] - side
     image = image[s1:s1+side, s2:s2+side]
-    image = cv2.resize(image, (96, 96))
-    img = image[...,::-1]
+    img = cv2.resize(image, (96, 96))
     img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
     x_train = np.array([img])
     embedding = model.predict_on_batch(x_train)
@@ -210,8 +208,7 @@ def img_to_encoding(image, model):
     s1 = int((image.shape[0] - side) / 2)
     s2 = int((image.shape[1] - side) / 2)
     image = image[s1:s1+side, s2:s2+side]
-    image = cv2.resize(image, (96, 96))
-    img = image[...,::-1]
+    img = cv2.resize(image, (96, 96))
     img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
     x_train = np.array([img])
     embedding = model.predict_on_batch(x_train)
@@ -219,8 +216,7 @@ def img_to_encoding(image, model):
 
 def img_to_encoding_range(sz, x, y, image, model):
     image = image[x:x+sz,y:y+sz]
-    image = cv2.resize(image, (96, 96))
-    img = image[...,::-1]
+    img = cv2.resize(image, (96, 96))
     img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
     x_train = np.array([img])
     embedding = model.predict_on_batch(x_train)
